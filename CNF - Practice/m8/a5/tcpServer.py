400701934396291c742ef1bfb5cec702c4467bca
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-12-18 17:18:43 +0530

    activity 5

---
+++ CNF - Practice/m8/a5/tcpServer.py
@@ -0,0 +1,27 @@
+import socket
+
+def main():
+	host = '127.0.0.1'
+	port = 5000
+
+	s = socket.socket()
+	s.bind((host, port))
+
+	s.listen(1)
+	c, addr = s.accept()
+	print ("connection from: " + str(addr))
+	while True:
+		data = c.recv(1024)
+		data = data.decode()
+		if not data:
+			break
+		print ("from connected user: " + str(data))
+		data = str(data).upper()
+		print ("sending: " + str(data))
+		c.send(data.encode())
+	c.close()
+
+if __name__ == '__main__':
+	main()
+
+
